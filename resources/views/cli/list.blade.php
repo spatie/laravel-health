7836440486becf0e47ca572357b74a92f8783739
freek <freek@spatie.be>
2021-11-26 17:26:30 +0100

    wip

--- resources/views/cli/list.blade.php
+++ resources/views/cli/list.blade.php
@@ -1,32 +1,32 @@
 <div class="m-1">
     @if(count($checkResults->storedCheckResults))
-    <div class="underline mb-1">Check results</div>
+        <div class="underline mb-1">Check results</div>
 
-    <div class="mb-1">
-    Last ran all the checks {{ $lastRanAt->diffForHumans() }}
-    </div>
+        <div class="mb-1">
+            Last ran all the checks {{ $lastRanAt->diffForHumans() }}
+        </div>
 
-    <table style="box">
-        <thead>
-        <tr>
-            <td></td>
-            <td>Check</td>
-            <td>Summary</td>
-            <td>Error message</td>
-        </tr>
-        </thead>
-        @foreach($checkResults->storedCheckResults as $result)
+        <table style="box">
+            <thead>
             <tr>
-                <td class="{{ $color($result->status) }}"></td>
-                <td>{{ $result->label }}</td>
-                <td>{{ $result->shortSummary }}</td>
-                <td>{{ $result->notificationMessage }}</td>
+                <td></td>
+                <td>Check</td>
+                <td>Summary</td>
+                <td>Error message</td>
             </tr>
-        @endforeach
-    </table>
+            </thead>
+            @foreach($checkResults->storedCheckResults as $result)
+                <tr>
+                    <td class="{{ $color($result->status) }}"></td>
+                    <td>{{ $result->label }}</td>
+                    <td>{{ $result->shortSummary }}</td>
+                    <td>{{ $result->notificationMessage }}</td>
+                </tr>
+            @endforeach
+        </table>
 
     @else
-        No checks have run yet...<br />
+        No checks have run yet...<br/>
         Please execute:
 
         php artisan health:run-checks
