4fd6b9d1ccafd1d606af23454ff8e577657d5a37
freekmurze <freekmurze@users.noreply.github.com>
2021-11-26 19:41:53 +0000

    Fix styling

--- tests/Http/Controllers/HealthCheckResultsControllerTest.php
+++ tests/Http/Controllers/HealthCheckResultsControllerTest.php
@@ -1,17 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use function Pest\Laravel\artisan;
+use function Pest\Laravel\get;
+use function Pest\Laravel\getJson;
 use Spatie\Health\Commands\RunChecksCommand;
 use Spatie\Health\Facades\Health;
 use Spatie\Health\Http\Controllers\HealthCheckResultsController;
 use Spatie\Health\Tests\TestClasses\FakeUsedDiskSpaceCheck;
-use function Pest\Laravel\artisan;
-use function Pest\Laravel\getJson;
-use function Pest\Laravel\get;
 use function Spatie\PestPluginTestTime\testTime;
 use function Spatie\Snapshots\assertMatchesSnapshot;
 
-beforeEach(function() {
+beforeEach(function () {
     testTime()->freeze('2021-01-01 00:00:00');
 
     Route::get('/', HealthCheckResultsController::class);
@@ -19,20 +19,20 @@ beforeEach(function() {
     $this->check = FakeUsedDiskSpaceCheck::new()->fakeDiskUsagePercentage(100);
 
     Health::checks([
-        $this->check
+        $this->check,
     ]);
 
     artisan(RunChecksCommand::class);
 });
 
-it('can display the results as html', function() {
+it('can display the results as html', function () {
     get('/')
         ->assertSuccessful()
         ->assertViewIs('health::list')
         ->assertSee($this->check->getLabel());
 });
 
-it('will display the results as json when the request accepts json', function() {
+it('will display the results as json when the request accepts json', function () {
     $json = getJson('/')
         ->assertSuccessful()
         ->json();
