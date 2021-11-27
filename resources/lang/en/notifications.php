f732a3d36384f95263aba201b181601e94dbe3e1
freek <freek@spatie.be>
2021-11-27 13:36:54 +0100

    wip

--- resources/lang/en/notifications.php
+++ resources/lang/en/notifications.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'checked_failed_subject' => 'Some of the health check on :application_name have failed',
+    'check_failed_subject' => 'Some of the health checks on :application_name have failed',
 
     'check_failed_body' => 'The following checks reported warnings and errors:'
 ];
