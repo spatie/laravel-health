738f92c9105d6b09e3b77c047810f1fc66cf2ee1
freekmurze <freekmurze@users.noreply.github.com>
2021-12-05 12:15:34 +0000

    Fix styling

--- tests/Http/Controllers/HealthCheckJsonResultsControllerTest.php
+++ tests/Http/Controllers/HealthCheckJsonResultsControllerTest.php
@@ -1,12 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Spatie\Health\ResultStores\StoredCheckResults\StoredCheckResults;
 use function Pest\Laravel\artisan;
 use function Pest\Laravel\getJson;
 use Spatie\Health\Commands\RunHealthChecksCommand;
 use Spatie\Health\Facades\Health;
 use Spatie\Health\Http\Controllers\HealthCheckJsonResultsController;
+use Spatie\Health\ResultStores\StoredCheckResults\StoredCheckResults;
 use Spatie\Health\Tests\TestClasses\FakeUsedDiskSpaceCheck;
 use function Spatie\PestPluginTestTime\testTime;
 use function Spatie\Snapshots\assertMatchesSnapshot;
