cb54b5d420f2b06eda975821d9f5168efec4c7e8
freek <freek@spatie.be>
2021-12-07 17:06:35 +0100

    wip

--- resources/views/list-cli.blade.php
+++ resources/views/list-cli.blade.php
@@ -1,10 +1,18 @@
-<div class="ml-2 my-1">
+<div class="mx-2 my-1">
     @if(count($checkResults?->storedCheckResults ?? []))
-        <div class="underline mb-1">Check results</div>
-
-        <div class="mb-1">
-            Last ran all the checks {{ $lastRanAt->diffForHumans() }}.
+        <div class="w-full text-white text-center bg-blue-800"></div>
+        <div class="w-full text-white bg-blue-800">
+            <span class="p-2 text-left w-1/2">Laravel Health Check Results</span>
+            <span class="p-2 text-right w-1/2">
+               Last ran all the checks
+                @if ($lastRanAt->diffInMinutes() < 1)
+                    just now
+                @else
+                    {{ $lastRanAt->diffForHumans() }}
+                @endif
+        </span>
         </div>
+        <div class="w-full text-white text-center bg-blue-800 mb-1"></div>
 
         <table style="box">
             <thead>
@@ -26,10 +34,10 @@
         </table>
     @else
         <div>
-        No checks have run yet...<br/>
-        Please execute:
+            No checks have run yet...<br/>
+            Please execute this command:
 
-        php artisan health:check-checks
+            php artisan health:run --fresh
         </div>
     @endif
 </div>
