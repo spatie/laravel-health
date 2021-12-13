49c569c39e1a4c954dc4dc33b03493f7cbf2bd7a
Jack <JackWH@users.noreply.github.com>
2021-12-13 18:30:41 +0000

    Add support for custom HealthCheckResultHistoryItems in EloquentHealthResultStore

--- src/ResultStores/EloquentHealthResultStore.php
+++ src/ResultStores/EloquentHealthResultStore.php
@@ -2,8 +2,10 @@
 
 namespace Spatie\Health\ResultStores;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
+use Spatie\Health\Exceptions\CouldNotSaveResultsInStore;
 use Spatie\Health\Checks\Result;
 use Spatie\Health\Models\HealthCheckResultHistoryItem;
 use Spatie\Health\ResultStores\StoredCheckResults\StoredCheckResult;
@@ -11,12 +13,33 @@ use Spatie\Health\ResultStores\StoredCheckResults\StoredCheckResults;
 
 class EloquentHealthResultStore implements ResultStore
 {
+    public static function determineHistoryItemModel(): string
+    {
+        $historyItemModel = config(
+        'health.result_stores.' . EloquentHealthResultStore::class . '.model',
+            HealthCheckResultHistoryItem::class,
+        );
+
+        if (! is_a($historyItemModel, HealthCheckResultHistoryItem::class, true)) {
+            throw CouldNotSaveResultsInStore::doesNotExtendHealthCheckResultHistoryItem($historyItemModel);
+        }
+
+        return $historyItemModel;
+    }
+
+    public static function getHistoryItemInstance(): HealthCheckResultHistoryItem
+    {
+        $historyItemClassName = self::determineHistoryItemModel();
+
+        return new $historyItemClassName();
+    }
+
     /** @param Collection<int, Result> $checkResults */
     public function save(Collection $checkResults): void
     {
         $batch = Str::uuid();
         $checkResults->each(function (Result $result) use ($batch) {
-            HealthCheckResultHistoryItem::create([
+            (self::determineHistoryItemModel())::create([
                 'check_name' => $result->check->getName(),
                 'check_label' => $result->check->getLabel(),
                 'status' => $result->status,
@@ -31,12 +54,12 @@ class EloquentHealthResultStore implements ResultStore
 
     public function latestResults(): ?StoredCheckResults
     {
-        if (! $latestItem = HealthCheckResultHistoryItem::latest()->first()) {
+        if (! $latestItem = (self::determineHistoryItemModel())::latest()->first()) {
             return null;
         }
 
         /** @var Collection<int, StoredCheckResult> $storedCheckResults */
-        $storedCheckResults = HealthCheckResultHistoryItem::query()
+        $storedCheckResults = (self::determineHistoryItemModel())::query()
             ->where('batch', $latestItem->batch)
             ->get()
             ->map(function (HealthCheckResultHistoryItem $historyItem) {
