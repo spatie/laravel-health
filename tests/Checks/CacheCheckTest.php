5698d71c4e166ea1f7fac05a2286bdc03ffafd82
Andy Hinkle <ahinkle10@gmail.com>
2021-12-16 22:06:23 -0600

    test: add supporting test for cache key check

--- tests/Checks/CacheCheckTest.php
+++ tests/Checks/CacheCheckTest.php
@@ -2,6 +2,7 @@
 
 use Spatie\Health\Checks\Checks\CacheCheck;
 use Spatie\Health\Enums\Status;
+use Spatie\Health\Tests\TestClasses\FakeCacheCheck;
 
 it('will determine that a working cache is ok', function () {
     $result = CacheCheck::new()->run();
@@ -18,4 +19,14 @@ it('will determine that a non-existing cache is not ok', function () {
     expect($result)
         ->status->toBe(Status::failed())
         ->getNotificationMessage()->toBe('An exception occurred with the application cache: `Cache store [does-not-exist] is not defined.`');
+});
+
+it('will return an error when it cannot set or retrieve cache key', function () {
+    $result = FakeCacheCheck::new()
+        ->replyWith(fn () => false)
+        ->run();
+
+    expect($result)
+        ->status->toBe(Status::failed())
+        ->notificationMessage->toBe('Could not set or retrieve an application cache value.');
 });
