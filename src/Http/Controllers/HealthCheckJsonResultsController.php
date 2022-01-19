a6709f56a69819bd34b59673b91436fc8486db61
M_A_X_NSK <hardstyle@myttk.ru>
2022-01-19 17:05:14 +0700

    Fixed json response header

--- src/Http/Controllers/HealthCheckJsonResultsController.php
+++ src/Http/Controllers/HealthCheckJsonResultsController.php
@@ -19,6 +19,7 @@ class HealthCheckJsonResultsController
         $checkResults = $resultStore->latestResults();
 
         return response($checkResults?->toJson() ?? '')
+            ->header('Content-Type', 'application/json')
             ->header('Cache-Control', 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
     }
 }
