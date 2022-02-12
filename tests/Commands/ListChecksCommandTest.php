66848be52de7fd3a47c58de5180c0dfd83f1ace3
Erik D'Ercole <erik.dercole@encodia.it>
2022-02-12 19:38:02 +0100

    Add --fail-command-on-failing-check to health:list artisan command

--- tests/Commands/ListChecksCommandTest.php
+++ tests/Commands/ListChecksCommandTest.php
@@ -16,3 +16,21 @@ it('thrown no exceptions with a check registered', function () {
 
     artisan(ListHealthChecksCommand::class, ['--fresh' => true])->assertSuccessful();
 });
+
+it('has an option that will let the command fail when a check fails', function () {
+    $fakeDiskSpaceCheck = FakeUsedDiskSpaceCheck::new();
+
+    Health::checks([
+        $fakeDiskSpaceCheck,
+    ]);
+
+    $fakeDiskSpaceCheck->fakeDiskUsagePercentage(0);
+    artisan('health:list')->assertSuccessful();
+    artisan('health:list --fail-command-on-failing-check')->assertSuccessful();
+
+    $fakeDiskSpaceCheck->fakeDiskUsagePercentage(100);
+
+    artisan('health:check')->assertSuccessful();
+    artisan('health:list')->assertSuccessful();
+    artisan('health:list --fail-command-on-failing-check')->assertFailed();
+});
