c496a1c6f682590a6a3045e982ab06d7ca821c34
Remzi <me@remzikocak.com>
2022-02-21 03:43:24 +0300

    Add German Translations

---
+++ resources/lang/de/notifications.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'laravel_health' => 'Laravel Gesundheit',
+
+    'check_failed_mail_subject' => 'Einige Gesundheits kontrollen auf :application_name haben fehlgeschlagen.',
+
+    'check_failed_mail_body' => 'Folgende kontrollen haben warnungen und fehler gemeldet:',
+
+    'check_failed_slack_message' => 'Einige Gesundheits kontrollen auf :application_name haben fehlgeschlagen.',
+
+    'health_results' => 'Gesundheitsergebnisse',
+
+    'check_results_from' => 'Gesundheitsergebnisse von',
+];
