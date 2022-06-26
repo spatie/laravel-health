ca752fc83077ff6574bb4325ab20665f9b2b58b2
Mohammad ALTAWEEL <hello@mohammad.tech>
2022-06-26 11:10:52 +0300

    Get the exception class name

--- src/Exceptions/CheckDidNotComplete.php
+++ src/Exceptions/CheckDidNotComplete.php
@@ -10,7 +10,7 @@ class CheckDidNotComplete extends Exception
     public static function make(Check $check, Exception $exception): self
     {
         return new self(
-            message: "The check named `{$check->getName()}` did not complete. An exception was thrown with this message: `{$exception->getMessage()}`",
+            message: "The check named `{$check->getName()}` did not complete. An exception was thrown with this message: `".get_class($exception).": {$exception->getMessage()}`",
             previous: $exception,
         );
     }
