b5a36ad3c743c46854d8c0142886f2091813373f
Md. Rakibul Islam <44586913+rakibdevs@users.noreply.github.com>
2022-06-30 15:41:54 +0600

    added translation for Bangla

---
+++ resources/lang/bn/notifications.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'laravel_health' => 'লারাভেল স্বাস্থ্য পরীক্ষা',
+
+    'check_failed_mail_subject' => ':application_name এর কিছু স্বাস্থ্য পরীক্ষা ব্যর্থ হয়েছে',
+
+    'check_failed_mail_body' => 'নিম্নলিখিত যাচাইকৃত সতর্কতা এবং ত্রুটি রিপোর্ট করা হয়েছে:',
+
+    'check_failed_slack_message' => ':application_name এর কিছু স্বাস্থ্য পরীক্ষা ব্যর্থ হয়েছে।',
+
+    'health_results' => 'স্বাস্থ্য পরীক্ষা ফলাফল',
+
+    'check_results_from' => 'এখান থেকে ফলাফল যাচাই করুন',
+];
