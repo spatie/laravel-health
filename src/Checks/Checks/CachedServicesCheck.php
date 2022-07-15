875e8c2a944770752fc7bf6d1c9996d44f4f65b7
freekmurze <freekmurze@users.noreply.github.com>
2022-07-15 10:29:56 +0000

    Fix styling

--- src/Checks/Checks/CachedServicesCheck.php
+++ src/Checks/Checks/CachedServicesCheck.php
@@ -7,23 +7,23 @@ use Spatie\Health\Checks\Result;
 
 class CachedServicesCheck extends Check
 {
-	public function run(): Result
-	{
-		$application = app();
-		$result = Result::make();
+    public function run(): Result
+    {
+        $application = app();
+        $result = Result::make();
 
-		if (!$application->configurationIsCached()) {
-			return $result->failed('Configs are not cached.');
-		}
+        if (! $application->configurationIsCached()) {
+            return $result->failed('Configs are not cached.');
+        }
 
-		if (!$application->routesAreCached()) {
-			return $result->failed('Routes are not cached.');
-		}
+        if (! $application->routesAreCached()) {
+            return $result->failed('Routes are not cached.');
+        }
 
-		if (!$application->eventsAreCached()) {
-			return $result->failed('Events are not cached.');
-		}
+        if (! $application->eventsAreCached()) {
+            return $result->failed('Events are not cached.');
+        }
 
-		return $result->ok();
-	}
+        return $result->ok();
+    }
 }
