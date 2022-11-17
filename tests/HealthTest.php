d45d9c3eb9ace0a1afe7cb30385e219fe61562d0
Luke Downing <lukeraymonddowning@gmail.com>
2022-11-17 10:40:58 +0000

    Adds the ability to fake registered checks.

--- tests/HealthTest.php
+++ tests/HealthTest.php
@@ -1,10 +1,14 @@
 <?php
 
+use Spatie\Health\Checks\Checks\DatabaseCheck;
 use Spatie\Health\Checks\Checks\PingCheck;
 use Spatie\Health\Checks\Checks\UsedDiskSpaceCheck;
+use Spatie\Health\Checks\Result;
+use Spatie\Health\Enums\Status;
 use Spatie\Health\Exceptions\DuplicateCheckNamesFound;
 use Spatie\Health\Exceptions\InvalidCheck;
 use Spatie\Health\Facades\Health;
+use Spatie\Health\Testing\FakeCheck;
 
 it('can register checks', function () {
     Health::checks([
@@ -44,3 +48,45 @@ it('will throw an exception when registering a string', function () {
         PingCheck::class,
     ]);
 })->throws(InvalidCheck::class);
+
+it('can fake checks', function () {
+    Health::checks([
+        DatabaseCheck::new(),
+        PingCheck::new(),
+    ]);
+
+    Health::fake([
+        DatabaseCheck::class => new Result(
+            Status::crashed(),
+            "We're just making sure faking works",
+            "Hey, faking works!",
+        ),
+        PingCheck::class => FakeCheck::result(
+            new Result(Status::warning()),
+            true
+        ),
+    ]);
+
+    $this->artisan('health:check')
+        ->expectsOutputToContain(ucfirst((string) Status::crashed()->value))
+        ->expectsOutputToContain(ucfirst((string) Status::warning()->value));
+});
+
+it('can pass a closure to fake checks', function () {
+    Health::checks([
+        DatabaseCheck::new()->name('MySQL')->connectionName('mysql'),
+        DatabaseCheck::new()->name('DB SQLite')->connectionName('sqlite'),
+    ]);
+
+    Health::fake([
+        DatabaseCheck::class => function (DatabaseCheck $check) {
+            return $check->getName() === 'MySQL'
+                ? new Result(Status::crashed())
+                : new Result(Status::warning());
+        }
+    ]);
+
+    $this->artisan('health:check')
+        ->expectsOutputToContain(ucfirst((string) Status::crashed()->value))
+        ->expectsOutputToContain(ucfirst((string) Status::warning()->value));
+});
