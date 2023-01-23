4e487e2ef0f8cae5ad49e2ffa6985f52b38a3426
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2023-01-23 17:28:04 +0100

    Add DatabaseSizeCheck

---
+++ tests/Checks/DatabaseSizeCheckTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Spatie\Health\Checks\Checks\DatabaseSizeCheck;
+use Spatie\Health\Enums\Status;
+
+it('will determine that database size is ok if it does not cross the maximum', function () {
+	$result = DatabaseSizeCheck::new()
+		->connectionName('mysql')
+		->failWhenSizeAboveGb(50)
+		->run();
+	
+	expect($result->status)->toBe(Status::ok());
+});
+
+it('will determine that database size is not ok if it does cross the maximum', function () {
+	$result = DatabaseSizeCheck::new()
+		->connectionName('mysql')
+		->failWhenSizeAboveGb(0)
+		->run();
+	
+	expect($result->status)->toBe(Status::failed());
+});
