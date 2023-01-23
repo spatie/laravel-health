e86dc5316a129c096f978a131da2067715ba4e9b
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2023-01-23 17:37:24 +0100

    Update DbConnectionInfo.php

--- src/Support/DbConnectionInfo.php
+++ src/Support/DbConnectionInfo.php
@@ -9,60 +9,61 @@ use Spatie\Health\Exceptions\DatabaseNotSupported;
 
 class DbConnectionInfo
 {
-	public function connectionCount(ConnectionInterface $connection): int
-	{
-		return match (true) {
-			$connection instanceof MySqlConnection => (int)$connection->selectOne($connection->raw('show status where variable_name = "threads_connected"'))->Value,
-			$connection instanceof PostgresConnection => (int)$connection->selectOne('select count(*) as connections from pg_stat_activity')->connections,
-			default => throw DatabaseNotSupported::make($connection),
-		};
-	}
+    public function connectionCount(ConnectionInterface $connection): int
+    {
+        return match (true) {
+            $connection instanceof MySqlConnection => (int)$connection->selectOne($connection->raw('show status where variable_name = "threads_connected"'))->Value,
+            $connection instanceof PostgresConnection => (int)$connection->selectOne('select count(*) as connections from pg_stat_activity')->connections,
+            default => throw DatabaseNotSupported::make($connection),
+        };
+    }
 
-	public function tableSizeInMb(ConnectionInterface $connection, string $table): float
-	{
-		$sizeInBytes = match (true) {
-			$connection instanceof MySqlConnection => $this->getMySQLTableSize($connection, $table),
-			$connection instanceof PostgresConnection => $this->getPostgresTableSize($connection, $table),
-			default => throw DatabaseNotSupported::make($connection),
-		};
+    public function tableSizeInMb(ConnectionInterface $connection, string $table): float
+    {
+        $sizeInBytes = match (true) {
+            $connection instanceof MySqlConnection => $this->getMySQLTableSize($connection, $table),
+            $connection instanceof PostgresConnection => $this->getPostgresTableSize($connection, $table),
+            default => throw DatabaseNotSupported::make($connection),
+        };
 
-		return $sizeInBytes / 1024 / 1024;
-	}
+        return $sizeInBytes / 1024 / 1024;
+    }
 
-	public function databaseSizeInMb(ConnectionInterface $connection): float
-	{
-		return match (true) {
-			$connection instanceof MySqlConnection => $this->getMySQlDatabaseSize($connection),
-			$connection instanceof PostgresConnection => $this->getPostgresDatabaseSize($connection),
-			default => throw DatabaseNotSupported::make($connection),
-		};
-	}
+    public function databaseSizeInMb(ConnectionInterface $connection): float
+    {
+        return match (true) {
+            $connection instanceof MySqlConnection => $this->getMySQlDatabaseSize($connection),
+            $connection instanceof PostgresConnection => $this->getPostgresDatabaseSize($connection),
+            default => throw DatabaseNotSupported::make($connection),
+        };
+    }
 
-	protected function getMySQLTableSize(ConnectionInterface $connection, string $table): int
-	{
-		return $connection->selectOne('SELECT (data_length + index_length) AS size FROM information_schema.TABLES WHERE table_schema = ? AND table_name = ?',
-			[
-				$connection->getDatabaseName(),
-				$table,
-			])->size;
-	}
+    protected function getMySQLTableSize(ConnectionInterface $connection, string $table): int
+    {
+        return $connection->selectOne('SELECT (data_length + index_length) AS size FROM information_schema.TABLES WHERE table_schema = ? AND table_name = ?', [
+            $connection->getDatabaseName(),
+            $table,
+        ])->size;
+    }
 
-	protected function getPostgresTableSize(ConnectionInterface $connection, string $table): int
-	{
-		return $connection->selectOne('SELECT pg_total_relation_size(?) AS size;', [
-			$table,
-		])->size;
-	}
+    protected function getPostgresTableSize(ConnectionInterface $connection, string $table): int
+    {
+        return $connection->selectOne('SELECT pg_total_relation_size(?) AS size;', [
+            $table,
+        ])->size;
+    }
 
-	protected function getMySQLDatabaseSize(ConnectionInterface $connection): int
-	{
-		return $connection->scalar('SELECT size_mb from (SELECT table_schema "name", ROUND(SUM(data_length + index_length) / 1024 / 1024, 1) "size_mb" FROM information_schema.tables GROUP BY table_schema) alias_one where name = ?', [$connection->getDatabaseName()]);
-	}
+    protected function getMySQLDatabaseSize(ConnectionInterface $connection): int
+    {
+        return $connection->scalar('SELECT size_mb from (SELECT table_schema "name", ROUND(SUM(data_length + index_length) / 1024 / 1024, 1) "size_mb" FROM information_schema.tables GROUP BY table_schema) alias_one where name = ?', [
+            $connection->getDatabaseName(),
+        ]);
+    }
 
-	protected function getPostgresDatabaseSize(ConnectionInterface $connection): int
-	{
-		return $connection->selectOne('SELECT pg_size_pretty(pg_database_size(?));', [
-			$connection->getDatabaseName()
-		])->size;
-	}
+    protected function getPostgresDatabaseSize(ConnectionInterface $connection): int
+    {
+        return $connection->selectOne('SELECT pg_size_pretty(pg_database_size(?));', [
+            $connection->getDatabaseName(),
+        ])->size;
+    }
 }
