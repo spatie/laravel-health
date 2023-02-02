ba9d08f6c10632a606290d594e2bd391e1e8709b
Stephan Fellhofer <stephan.fellhofer@code-box.at>
2023-02-02 11:50:25 +0100

    Fix `DatabaseSizeCheck` to not send notifications on passed checks
    The current database size usage is now included in the short summary
    instead of the `$result->ok()`.

---
+++ tests/TestClasses/FakeDatabaseSizeCheck.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Spatie\Health\Tests\TestClasses;
+
+use Closure;
+use Spatie\Health\Checks\Checks\DatabaseSizeCheck;
+
+class FakeDatabaseSizeCheck extends DatabaseSizeCheck
+{
+    protected float $fakeDatabaseSizeInGb = 0;
+
+    public function fakeDatabaseSizeInGb(float $fakeDatabaseSizeInGb): self
+    {
+        $this->fakeDatabaseSizeInGb = $fakeDatabaseSizeInGb;
+
+        return $this;
+    }
+
+    public function getDatabaseSizeInGb(): float
+    {
+        return $this->fakeDatabaseSizeInGb;
+    }
+}
