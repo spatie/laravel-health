ba03f45d8ee1a4cdc0d6fa302280c4d926a52cc9
Michael Nabil <46572405+michaelnabil230@users.noreply.github.com>
2023-12-18 12:41:48 +0200

    Fix language

--- resources/lang/ar/notifications.php
+++ resources/lang/ar/notifications.php
@@ -3,13 +3,13 @@
 return [
     'laravel_health' => 'Laravel Health',
 
-    'check_failed_mail_subject' => 'بعض التشخيصات في :application_name فشلت',
+    'check_failed_mail_subject' => 'فشل بعض فحوص الصحة على :application_name',
 
-    'check_failed_mail_body' => 'التشخيصات التالية أبلغت عن تحذيرات و أخطاء',
+    'check_failed_mail_body' => 'الفحوص التالية قد أبلغت عن تحذيرات وأخطاء:',
 
-    'check_failed_slack_message' => 'بعض التشخيصات في :application_name فشلت',
+    'check_failed_slack_message' => 'فشل بعض فحوص الصحة على :application_name.',
 
-    'health_results' => 'نتائج التشخيص',
+    'health_results' => 'نتائج الصحة',
 
-    'check_results_from' => 'تحقق من النتائج عن طريق',
+    'check_results_from' => 'نتائج الفحص من',
 ];
