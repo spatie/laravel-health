44fe673100f1a0868764b0a4fc4c488163436702
Michael Nabil <46572405+michaelnabil230@users.noreply.github.com>
2023-12-18 13:08:03 +0200

    Update notifications.php

--- resources/lang/ar/notifications.php
+++ resources/lang/ar/notifications.php
@@ -3,13 +3,13 @@
 return [
     'laravel_health' => 'Laravel Health',
 
-    'check_failed_mail_subject' => 'فشل بعض فحوص الصحة على :application_name',
+    'check_failed_mail_subject' => 'لقد فشلت بعض عمليات التحقق من السلامة على :application_name',
 
-    'check_failed_mail_body' => 'الفحوص التالية قد أبلغت عن تحذيرات وأخطاء:',
+    'check_failed_mail_body' => 'أبلغت عمليات التحقق التالية عن تحذيرات وأخطاء:',
 
-    'check_failed_slack_message' => 'فشل بعض فحوص الصحة على :application_name.',
+    'check_failed_slack_message' => 'لقد فشلت بعض عمليات التحقق من السلامة على :application_name.',
 
     'health_results' => 'نتائج الصحة',
 
-    'check_results_from' => 'تحقق من النتائج عن طريق',
+    'check_results_from' => 'التحقق من النتائج من',
 ];
