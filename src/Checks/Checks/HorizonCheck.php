c0966f3385c6988d6213edc7629000ebfcc4a922
Nicholas Bell <nick@advantagelearn.com>
2024-11-08 01:09:17 +0200

    update checks to use pingable and ping config urls if set

--- src/Checks/Checks/HorizonCheck.php
+++ src/Checks/Checks/HorizonCheck.php
@@ -6,9 +6,12 @@ use Exception;
 use Laravel\Horizon\Contracts\MasterSupervisorRepository;
 use Spatie\Health\Checks\Check;
 use Spatie\Health\Checks\Result;
+use Spatie\Health\Traits\Pingable;
 
 class HorizonCheck extends Check
 {
+    use Pingable;
+
     public function run(): Result
     {
         $result = Result::make();
@@ -33,6 +36,10 @@ class HorizonCheck extends Check
             return $result
                 ->warning('Horizon is running, but the status is paused.')
                 ->shortSummary('Paused');
+        }
+
+        if (config('health.horizon.heartbeat_url')) {
+            $this->pingUrl(config('health.horizon.heartbeat_url'));
         }
 
         return $result->ok()->shortSummary('Running');
